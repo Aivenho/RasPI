0e6ea47f0158d2e7ac8f91b959387029f3a7350b
Aivenho <nauris.sedlers@gmail.com>
2017-10-13 17:51:38 +0300

    Update readtemp.py

--- BattTempMon/readtemp.py
+++ BattTempMon/readtemp.py
@@ -1,16 +1,17 @@
 import os
 import glob
 import time
-from datetime import datetime
+import datetime
 
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
+
 device_folder = ''
 evice_file = ''
 
 def read_temps_multi():
         base_dir = '/sys/bus/w1/devices/'
-        for device in glob.glob(base_dir + '28*'):
+        for device in glob.glob(base_dir + '10*'):
                 global device_folder
                 global device_file
                 device_folder = device
@@ -34,9 +35,8 @@ def read_temp():
     if equals_pos != -1:
         temp_string = lines[1][equals_pos+2:]
         temp_c = float(temp_string) / 1000.0
-        temp_f = temp_c * 9.0 / 5.0 + 32.0
-        return date, sensor, temp_c, temp_f
+        return date, sensor, temp_c
 
 while True:
         read_temps_multi()
-        time.sleep(1)
+        time.sleep(3)
